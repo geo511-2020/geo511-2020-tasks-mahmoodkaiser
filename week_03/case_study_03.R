ce9630df533758c15e49bb334c6e0fceb3908252
Kaiser Mahmood <mahmoodkaiser@gmail.com>
2020-12-17 12:58:34 -0500

    final commit

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -7,32 +7,34 @@ library(dplyr)
 
 #removing Kuwait
 gapminder <- gapminder %>%
-  filter(country!="Kuwait")
-
-gapminder
+  filter(country != "Kuwait")
 
 #Plot 1
-ggplot(gapminder,aes(lifeExp,gdpPercap,color=continent, size=pop/100000)) +
+plot1 <- ggplot(gapminder, aes(lifeExp, gdpPercap, color = continent, size = pop/100000)) +
   geom_point() +
   scale_y_continuous(trans = "sqrt") +
-  facet_wrap(~year,nrow = 1) +
+  facet_wrap(~year, nrow = 1)+
   theme_bw() +
-  labs(x="Life Expectancy",y="GDP per capita")
+  labs(x = "Life Expectancy",y = "GDP per capita")
 ggsave("Case_study_3_plot1.png", width = 17, height = 10, units = "cm")
 
-#plot 2
-gapminder_continent <- gapminder %>%
-  group_by(year,continent) %>%
-  summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
-            pop = sum(as.numeric(pop)))
-gapminder_continent
+print(plot1)
 
-ggplot(gapminder_continent,aes(year,gdpPercapweighted,color=continent, size=pop/100000)) +
-  geom_line() +
-  geom_point() +
-  geom_line(data=gapminder,aes(year,gdpPercap,color= "black", group = country)) +
-  geom_point(data=gapminder,aes(year,gdpPercap,color= "black", size=pop/100000)) +
-  facet_wrap(~continent,nrow=1) +
+#Plot 2
+gapminder_continent <- gapminder %>%
+  group_by(year, continent) %>%
+  summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop), 
+            pop = sum(as.numeric(pop)))
+
+plot2 <- ggplot(gapminder, aes(year, gdpPercap, color = continent)) + 
+  geom_line(aes(group = country)) + 
+  geom_point(aes(size = pop/100000, group = country)) + 
+  geom_line(gapminder_continent, color = "black", aes(year, gdpPercapweighted)) + 
+  geom_point(gapminder_continent, color = "black", aes(year, gdpPercapweighted, size = pop/100000)) +
+  facet_wrap(~continent, nrow = 1) +
   theme_bw() +
-  labs(x= "Year", y="GDP per capita")
-ggsave("Case_study_3_plot2.png", width = 17, height = 10, units = "cm") 
+  labs(x = "Year", y = "GDP per capita")
+ggsave("Case_study_3_plot2.png", width = 17, height = 10, units = "cm") #took help from brendan for ggsave
+print(plot2)
+
+#plot 2 wasn't working. Re-shuffed some aesthetics from the 1st commit.
